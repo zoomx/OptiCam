0d888cab6e6ea57913c0c9e192d238bd0767f294
zoomx <zoomx@tiscalinet.it>
2016-06-23 18:55:03 +0200

    Add files via upload
    First commit

---
+++ OptiCam/OptiCam/fMain.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class fMain
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla nell'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 115200
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(24, 28)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(150, 150)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(197, 28)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(197, 66)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(74, 22)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(197, 120)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(74, 22)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'fMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "fMain"
+        Me.Text = "OptiCam"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+
+End Class
---
+++ OptiCam/OptiCam/fMain.vb
@@ -0,0 +1,55 @@
+﻿Public Class fMain
+    Public DrawArea As Bitmap
+    Private Sub fMain_Load(sender As Object, e As System.EventArgs) Handles Me.Load
+
+
+        DrawArea = New Bitmap(PictureBox1.Size.Width, PictureBox1.Size.Height)
+        PictureBox1.Image = DrawArea
+    End Sub
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim g As Graphics
+        DrawArea = New Bitmap(PictureBox1.Size.Width, PictureBox1.Size.Height)
+        DrawArea.SetPixel(100, 100, Color.Black)
+        g = Graphics.FromImage(DrawArea)
+        Dim mySolidColorBrush As SolidBrush = Brushes.Black
+        'mySolidColorBrush.Color = Color.Aqua
+        Dim mypen As Pen = New Pen(Color.Black)
+        Dim mybrush As Brush
+        mybrush = Brushes.Black
+        g.FillRectangle(mybrush, 100, 100, 10, 10)
+        Dim mycolor As Color
+        mycolor = Color.FromArgb(10, 10, 10)
+        mySolidColorBrush.Color = Color.FromArgb(128, 128, 128)
+        mybrush = mySolidColorBrush
+        g.FillRectangle(mybrush, 120, 120, 10, 10)
+        'mypen.Brush = Brushes.Black
+        mypen.Width = 2
+        'mycolor=(10,10,10,10)
+        'mypen.Color =
+        g.DrawLine(mypen, 0, 0, 15, 15)
+
+        PictureBox1.Image = DrawArea
+        PictureBox1.Refresh()
+        g.Dispose()
+    End Sub
+
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+        Dim g As Graphics
+        PictureBox1.Refresh()
+        g = PictureBox1.CreateGraphics
+        Dim mypen As Pen = New Pen(Color.Black, 2)
+        'mypen.Brush = Brushes.Black
+        'mypen.Width = 2
+
+        g.DrawLine(mypen, 0, 0, 15, 15)
+        'g.Dispose()
+    End Sub
+
+    Private Sub Button3_Click(sender As System.Object, e As System.EventArgs) Handles Button3.Click
+        'capture
+        'wait for startline
+        'get entire line and split in 15
+        'draw 15 ppoint
+    End Sub
+End Class
